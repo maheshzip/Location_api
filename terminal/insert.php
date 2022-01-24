77f1823519cb93244af27a9ffe27e3a366b97901
Oblivi8 <97178181+Oblivi8@users.noreply.github.com>
2022-01-24 22:06:20 +0530

    Add files via upload

---
+++ terminal/insert.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert Data</title>
+</head>
+<body>
+    <?php
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = 'test';
+
+        //Create connection
+        $mysqli = new mysqli($server, $username, $password, $dbname);
+
+        //check connection
+        if ($mysqli->connect_error)
+        {
+            die("Connection error: " . $mysqli->connect_error);
+        }
+
+        $query = "INSERT INTO table1 (protocol, port, agent, ref, hostname)
+        VALUES ('wihsjk','jfhdsjfhdsk','gajdh','hgdfhdfk','ddghjdg')";
+
+        if ($mysqli->query($query)===TRUE){
+            echo "New record created successfully";
+        }else{
+            echo "Error: " . $mysqli->error;
+        }
+
+        $mysqli->close();
+
+    ?>
+</body>
+</html>
