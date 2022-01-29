433e2ed6a6d5756370b654c14b7a8910f4392f96
maheshzip <maheshpimparkar6@gmail.com>
2022-01-29 15:29:02 +0530

    stfu

--- terminal/admin/index.php
+++ terminal/admin/index.php
@@ -1,11 +1,68 @@
-<!DOCTYPE html>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <html>
-        <center>
-            <h3>
-                ADMIN PANEL
-            </h3>
-        </center>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>ADMIN PANEL</title>
+  </head>
+  <body>
+    
+  <div class="container-fluid">
+
+    <center style="
+margin-left: 200px;
+margin-right: 200px;
+">
+<form style="
+margin-left: 100px;
+margin-right: 100px;
+">
+<div class="row mb-3">
+  <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+  <div class="col-sm-10">
+    <input type="email" class="form-control" id="inputEmail3">
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+  <div class="col-sm-10">
+    <input type="password" class="form-control" id="inputPassword3">
+  </div>
+</div>
+<fieldset class="row mb-3">
+      <div class="col-sm-10">
+          <left>
+          <div class="form-check">
+      <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked="">
+      <label class="form-check-label" for="gridRadios1">
+        Remember me!
+      </label>
+    </div>
+
+          </left>
+   
+    
+    
+  </div>
+</fieldset>
+
+<button type="submit" class="btn btn-primary">LOG IN</button>
+</form>
+</center>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 
-    </html>
+    </div>
+
+  </body>
+</html>
